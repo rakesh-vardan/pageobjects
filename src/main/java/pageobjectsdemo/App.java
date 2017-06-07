79e2055aacb943931fe61deed5aacb09b868108c
rakeshb <rakeshlearnit@gmail.com>
2017-06-07 16:53:11 +0530

    Initial commit

---
+++ src/main/java/pageobjectsdemo/App.java
@@ -0,0 +1,13 @@
+package pageobjectsdemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
